2711cc73156431a362383c82628d943d0826d2e3
Andrey Grunenkov <60237240+gras4all@users.noreply.github.com>
2020-09-20 17:00:43 +0300

    homework 3 fixes

--- Course5Homework/Screens/ReposList/Models/Repo.swift
+++ Course5Homework/Screens/ReposList/Models/Repo.swift
@@ -15,19 +15,4 @@ struct Repo: Decodable {
     var description: String
     var owner: RepoOwner
     
-    enum CodingKeys: String, CodingKey {
-        case id
-        case name
-        case description
-        case owner
-    }
-    
-    init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        self.id = try container.decode(Int.self, forKey: .id)
-        self.name = try container.decode(String.self, forKey: .name)
-        self.description = try container.decode(String.self, forKey: .description)
-        self.owner = try container.decode(RepoOwner.self, forKey: .owner)
-    }
-    
 }
